5cfbee8e0215fe101d2e1caaf308f3815bcc3669
Raül de Arriba <rauldearriba@gmail.com>
2022-11-17 17:57:59 +0100

    prepare auth

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,11 +3,34 @@ import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout'
 
+import { useForm } from '../../hooks'
+import { useDispatch } from 'react-redux'
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth'
+
 
 export const LoginPage = () => {
+
+  const dispatch = useDispatch();
+  const {email, password, onInputChange} = useForm({
+    email: 'raul@google.com',
+    password: '123456'
+  });
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    dispatch(checkingAuthentication() );
+
+  }
+
+  const onGoogleSignIn = () => {
+    dispatch( startGoogleSignIn() );
+
+
+  }
+
   return (
     <AuthLayout title='Login'>
-      <form>
+      <form onSubmit={ onSubmit }>
             <Grid container>
               <Grid item xs={ 12 } sx={{ mt: 2}}>
                 <TextField
@@ -15,6 +38,9 @@ export const LoginPage = () => {
                   type='email'
                   placeholder='correo@google.com'
                   fullWidth
+                  name='email'
+                  value={ email }
+                  onChange={ onInputChange }
                   />
               </Grid>
               <Grid item xs={ 12 } sx={{ mt: 2}}>
@@ -23,17 +49,24 @@ export const LoginPage = () => {
                   type='password'
                   placeholder='contraseña'
                   fullWidth
+                  name='password'
+                  value={ password }
+                  onChange={ onInputChange }
                   />
               </Grid>
 
               <Grid container spacing={ 2 } sx={{ mb: 2, mt:1}}>
                 <Grid item xs={ 12 } sm={ 6 }>
-                  <Button variant='contained' fullWidth>
+                  <Button type='submit' variant='contained' fullWidth>
                     Login
                   </Button>
                 </Grid>
                 <Grid item xs={ 12 } sm={ 6 }>
-                  <Button variant='contained' fullWidth>
+                  <Button
+                    variant='contained' 
+                    fullWidth
+                    onClick={onGoogleSignIn}
+                  >
                     <Google />
                     <Typography sx={{ ml:1 }}>Google</Typography>
                   </Button>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { CabonorteApp } from './CabonorteApp';
+import { store } from './store'
 import './styles.css'
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CabonorteApp />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <CabonorteApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
